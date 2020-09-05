62f3b762a0864a5669723ed2bb08b39b875c1055
massimo trovati <meshmi@outlook.it>
2020-09-05 10:12:05 +0200

    Lucky-Numbers-Win32

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Lucky_Number_Win32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Coral;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(176, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 65);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "90";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(264, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 65);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "90";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(352, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 65);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "90";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(440, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 65);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "90";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Print", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(528, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 65);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "90";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe Print", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(616, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 65);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "90";
+            this.label6.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 72);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Lucky-Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lucky_Number_Win32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // creates a number between 1 and 90
+            Random numerocasuale = new Random();
+            int numero1 = Convert.ToInt32(numerocasuale.Next(1, 90).ToString());
+            int numero2 = Convert.ToInt32(numerocasuale.Next(1, 90).ToString());
+            int numero3 = Convert.ToInt32(numerocasuale.Next(1, 90).ToString());
+            int numero4 = Convert.ToInt32(numerocasuale.Next(1, 90).ToString());
+            int numero5 = Convert.ToInt32(numerocasuale.Next(1, 90).ToString());
+            int numero6 = Convert.ToInt32(numerocasuale.Next(1, 90).ToString());
+            //Show Number
+            //1
+            string replace1 = label1.Text.Replace(label1.Text, "" + numero1);
+            label1.Text = replace1;
+            label1.Visible = true;
+            Thread.Sleep(500);
+            //2
+            string replace2 = label2.Text.Replace(label2.Text, "" + numero2);
+            label2.Text = replace2;
+            label2.Visible = true;
+            Thread.Sleep(500);
+            //3
+            string replace3 = label3.Text.Replace(label3.Text, "" + numero3);
+            label3.Text = replace3;
+            label3.Visible = true;
+            Thread.Sleep(500);
+            //4
+            string replace4 = label4.Text.Replace(label4.Text, "" + numero4);
+            label4.Text = replace4;
+            label4.Visible = true;
+            Thread.Sleep(500);
+            //5
+            string replace5 = label5.Text.Replace(label5.Text, "" + numero5);
+            label5.Text = replace5;
+            label5.Visible = true;
+            Thread.Sleep(500);
+            //6
+            string replace6 = label6.Text.Replace(label6.Text, "" + numero6);
+            label6.Text = replace6;
+            label6.Visible = true;
+            Thread.Sleep(500);
+        }
+    }
+}
